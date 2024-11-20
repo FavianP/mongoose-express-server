3aa90b56fef592a0a528c14b550fc839f11a2a5f
FavianP <142631192+FavianP@users.noreply.github.com>
2024-11-20 20:58:57 +0000

    red

--- models/GroceryItem.js
+++ models/GroceryItem.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 const grocerySchema = new mongoose.Schema({
-    item: { type: String },
-    food_group: {type: String}
+    item: {
+        type: String,
+        required: [true,'item requires a string value']
+    },
+    food_group: {type: String, required:[true,'item requires a string value']}
+
 });
 
 module.exports = mongoose.model('GroceryItem', grocerySchema);
